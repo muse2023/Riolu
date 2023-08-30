d1815804ddb4d73e50077d5eba0c2b4268eef583
muse2023 <pandasuperfans@gmail.com>
2023-08-31 01:47:01 +0800

    单纯开个头，login是乱写的

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<script setup lang="js">
+import Login from './components/login.vue'
+</script>
+<template>
+  <Login />
+</template>
---
+++ src/views/Login.vue
@@ -0,0 +1,9 @@
+<script setup lang="js">
+import Login from '../components/login.vue'
+</script>
+
+<template>
+  <main>
+    <Login />
+  </main>
+</template>
